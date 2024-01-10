02f9b6e1042119ad7ed0b093e87c69fbcb087cf4
Francisco Madeira <francisco.madeira@diconium.com>
2024-01-10 11:24:56 +0000

    build: updated webpack config for edge and browser build

--- ethereal-nexus-dashboard/next.config.js
+++ ethereal-nexus-dashboard/next.config.js
@@ -1,16 +1,26 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   output: "standalone",
-  webpack: (config, { isServer }) => {
+  webpack: (config, { webpack, isServer, nextRuntime }) => {
     if (!isServer) {
       config.resolve.fallback = {
         net: false,
         tls: false,
         perf_hooks: false,
-        fs: false
+        fs: false,
       };
     }
 
-    return config;
+    if(nextRuntime === 'edge') {
+      config.plugins.push(new webpack.IgnorePlugin({
+        resourceRegExp: /^cloudflare:sockets$/,
+      }))
+
+      config.plugins.push(new webpack.NormalModuleReplacementPlugin(/^node:stream$/, (resource) => {
+        resource.request = resource.request.replace(/^node:stream$/, 'readable-stream');
+      }))
+    }
+
+    return config
   },
 };

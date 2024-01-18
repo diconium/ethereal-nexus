99dade3ff9f1a3ed5b19dfb242bda13f26afbf3f
Francisco Madeira <francisco.madeira@diconium.com>
2024-01-18 18:24:48 +0000

    refactor: replace projects data source

--- ethereal-nexus-dashboard/src/app/projects/page.tsx
+++ ethereal-nexus-dashboard/src/app/projects/page.tsx
@@ -1,7 +1,13 @@
 import React from "react";
-import { ProjectsDataTable } from "@/components/projects/projects-data-table/data-table";
+import { DataTable } from '@/components/ui/data-table/data-table';
+import { getProjects } from '@/data/projects/actions';
+import { auth } from '@/auth';
+import { columns } from '@/components/projects/table/columns';
 
 export default async function Projects() {
+  const session = await auth()
+  const projects = await getProjects(session?.user?.id);
+
   return (
     <div className="container h-full flex-1 flex-col space-y-8 p-8 md:flex">
       <div className="flex items-center justify-between space-y-2">
@@ -10,7 +16,16 @@ export default async function Projects() {
           <p className="text-muted-foreground">Manage your projects here!</p>
         </div>
       </div>
-      <ProjectsDataTable />
+      {
+        projects.success ?
+          <DataTable
+            entity={'projects'}
+            handlerPath={'/users/new'}
+            columns={columns}
+            data={projects.data}
+          /> :
+          projects.error.message
+      }
     </div>
   );
 }

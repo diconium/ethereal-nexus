6708401badbc65434236ac72f1479ab9129f040f
Francisco Madeira <francisco.madeira@diconium.com>
2024-01-18 18:12:45 +0000

    chore: fix drizzle config file

--- ethereal-nexus-dashboard/drizzle.config.ts
+++ ethereal-nexus-dashboard/drizzle.config.ts
@@ -4,13 +4,17 @@ import * as dotenv from 'dotenv';
 dotenv.config({
   path: '.env.local',
 });
+
 export default {
-  schema: "./src/data/**/schema.ts",
+  verbose: true,
+  schema: [
+    "src/data/**/schema.ts",
+  ],
   out: "./drizzle",
   driver: 'pg',
   dbCredentials: {
     host: process.env.PGHOST!,
-    port: Number(process.env.PGPOR),
+    port: Number(process.env.PGPORT),
     user: process.env.PGUSER,
     password: process.env.PGPASSWORD,
     database: 'postgres',

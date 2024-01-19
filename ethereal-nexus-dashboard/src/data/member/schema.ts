4dc42cea2f477d97bd7aee858d6add44466cfd58
Francisco Madeira <francisco.madeira@diconium.com>
2024-01-19 13:30:05 +0000

    feat: bring projects page to postgres

--- ethereal-nexus-dashboard/src/data/member/schema.ts
+++ ethereal-nexus-dashboard/src/data/member/schema.ts
@@ -1,21 +1,27 @@
-import { pgTable, text, uuid } from 'drizzle-orm/pg-core';
+import { pgTable, primaryKey, text, uuid } from 'drizzle-orm/pg-core';
 import { users } from '@/data/users/schema';
 import { projects } from '@/data/projects/schema';
 import { relations } from 'drizzle-orm';
 
-export const members = pgTable("member", {
-  id: uuid('id').defaultRandom(),
-  user_id: text("user_id").notNull().references(() => users.id),
-  resource: text("resource").notNull().references(() => projects.id, { onDelete: 'cascade' }),
-})
+export const members = pgTable('member', {
+    id: uuid('id').notNull().defaultRandom(),
+    user_id: text('user_id').notNull().references(() => users.id),
+    resource: text('resource').notNull().references(() => projects.id, { onDelete: 'cascade' })
+  },
+  (table) => {
+    return {
+      pk: primaryKey({ columns: [table.user_id, table.resource] })
+    };
+  }
+);
 export const membersRelations = relations(members, ({ one }) => ({
   user: one(users, {
     fields: [members.user_id],
-    references: [users.id],
+    references: [users.id]
   }),
   resource: one(projects, {
     fields: [members.resource],
-    references: [projects.id],
-  }),
+    references: [projects.id]
+  })
 }));
 

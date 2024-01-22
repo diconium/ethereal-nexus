3a63fdd6062e6730c709ed870185486e4dcd9ffa
Francisco Madeira <francisco.madeira@diconium.com>
2024-01-22 14:19:03 +0000

    feat: update OAS for /projects route

--- ethereal-nexus-dashboard/src/data/projects/dto.ts
+++ ethereal-nexus-dashboard/src/data/projects/dto.ts
@@ -1,17 +1,55 @@
-import { projectComponentConfig, projects, projectsRelations } from './schema';
-import { members } from '@/data/member/schema';
-import { memberSchema } from '@/data/member/dto';
+import { projectComponentConfig, projects } from './schema';
 import { createInsertSchema, createSelectSchema } from 'drizzle-zod';
 import { componentsSchema } from '@/data/components/dto';
 import { z } from 'zod';
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Project:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: ID of the project
+ *         name:
+ *           type: string
+ *           description: Name of the project
+ *         description:
+ *           type: string
+ *           description: Description of the project
+ */
 export const projectSchema = createSelectSchema(projects);
+export type Project = z.infer<typeof projectSchema>
 
 export const projectComponentConfigSchema = createSelectSchema(projectComponentConfig);
+export type ProjectComponentConfig = z.infer<typeof projectComponentConfigSchema>
 
-export const projectWithComponentSchema = projectSchema.extend({
-  components: projectComponentConfigSchema.pick({ component_id: true }).array(),
-});
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     ProjectWithComponentId:
+ *       allOf:
+ *         - $ref: '#/components/schemas/Project'
+ *         - type: object
+ *           properties:
+ *             components:
+ *               type: array
+ *               description: ID of the project
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   component_id: string
+ */
+export const projectWithComponentIdSchema = projectSchema
+  .extend({
+    components: projectComponentConfigSchema
+      .pick({ component_id: true })
+      .array(),
+  });
+export type ProjectWithComponentId = z.infer<typeof projectWithComponentIdSchema>
 
 export const projectComponentsSchema = projectSchema
   .pick({ name: true })
@@ -20,9 +58,27 @@ export const projectComponentsSchema = projectSchema
       component: componentsSchema
     }).array()
   });
+export type ProjectComponent = z.infer<typeof projectComponentsSchema>
 
-export const newProjectSchema = createInsertSchema(projects, {
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     ProjectInput:
+ *       type: object
+ *       properties:
+ *         name:
+ *           type: string
+ *           description: Name of the project
+ *         description:
+ *           type: string
+ *           description: Description of the project
+ */
+export const projectInputSchema = createInsertSchema(projects, {
   name: (schema) =>
-    schema.name.min(4, 'Name must be longer than 4 characters.')
-})
+    schema.name
+      .min(4, 'Name must be longer than 4 characters.')
+  })
+  .omit({id: true})
   .required({ name: true });
+export type ProjectInput = z.infer<typeof projectInputSchema>
--- ethereal-nexus-dashboard/src/lib/api-doc/api-doc.service.ts
+++ ethereal-nexus-dashboard/src/lib/api-doc/api-doc.service.ts
@@ -4,7 +4,10 @@ import "server-only";
 
 export const getSwaggerSpec = async () => {
   const spec: Record<string, any> = createSwaggerSpec({
-    apiFolder: "src/app/api/v1", // define api folder under app folder
+    apiFolder: "src/app/api/v1",
+    schemaFolders: [
+      "src/data"
+    ],
     definition: {
       openapi: "3.0.0",
       info: {

3cf7497dba0fd9ca614a7eb2857587f3adf930c7
Francisco Madeira <francisco.madeira@diconium.com>
2024-01-29 13:20:02 +0000

    feat: add link to user settings

--- ethereal-nexus-dashboard/src/app/users/[id]/page.tsx
+++ ethereal-nexus-dashboard/src/app/users/[id]/page.tsx
@@ -3,6 +3,9 @@ import { auth } from '@/auth';
 import { notFound } from 'next/navigation';
 import { getUserById } from '@/data/users/actions';
 import { ApiKeyList } from '@/components/user/api-key-table/api-key-list';
+import { DataTable } from '@/components/ui/data-table/data-table';
+import { columns } from '@/components/projects/table/columns';
+import React from 'react';
 
 export default async function UserPage({ params: { id } }: any) {
   const session = await auth();
@@ -13,12 +16,12 @@ export default async function UserPage({ params: { id } }: any) {
   }
 
   return (
-    <div className="container space-y-6">
-      <div>
-        <h3 className="text-lg font-medium">Welcome {user.data.name}</h3>
-        <p className="text-sm text-muted-foreground">
-          Personal Data
-        </p>
+    <div className="container h-full flex-1 flex-col space-y-8 p-8 md:flex">
+      <div className="flex items-center justify-between space-y-2">
+        <div>
+          <h2 className="text-2xl font-bold tracking-tight">Welcome {user.data.name}</h2>
+          <p className="text-muted-foreground">User Settings</p>
+        </div>
       </div>
       <Separator />
       <ApiKeyList />

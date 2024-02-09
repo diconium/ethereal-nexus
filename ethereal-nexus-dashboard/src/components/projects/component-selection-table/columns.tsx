9622a1f0c3170d1500c4fc199f4e8b15a98f1616
Francisco Madeira <francisco.madeira@diconium.com>
2024-02-09 10:41:14 +0000

    feat: fix components list page

--- ethereal-nexus-dashboard/src/components/projects/component-selection-table/columns.tsx
+++ ethereal-nexus-dashboard/src/components/projects/component-selection-table/columns.tsx
@@ -4,8 +4,30 @@ import * as React from 'react';
 import { DataTableColumnHeader } from '@/components/ui/data-table/data-table-column-header';
 import { Switch } from '@/components/ui/switch';
 import { ActiveSwitch } from '@/components/projects/component-selection-table/active-switch';
+import { Checkbox } from '@/components/ui/checkbox';
 
 export const columns = [
+  {
+    id: 'select',
+    header: ({ table }) => (
+      <Checkbox
+        checked={table.getIsAllPageRowsSelected()}
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+        className="translate-y-[2px]"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+        className="translate-y-[2px]"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false
+  },
   {
     id: 'name',
     accessorFn: row => row.name,

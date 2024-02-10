e586dd7371752089f93cf4e20698a84a36ff22f3
Francisco Madeira <francisco.madeira@diconium.com>
2024-02-10 19:19:55 +0000

    chore: update DB connections config

--- ethereal-nexus-dashboard/next.config.js
+++ ethereal-nexus-dashboard/next.config.js
@@ -1,22 +1,4 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   output: "standalone",
-  webpack: (config, { webpack, isServer, nextRuntime }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        net: false,
-        tls: false,
-        perf_hooks: false,
-        fs: false,
-      };
-    }
-
-    if(nextRuntime === 'edge') {
-      config.plugins.push(new webpack.IgnorePlugin({
-        resourceRegExp: /^cloudflare:sockets$/,
-      }))
-    }
-
-    return config
-  },
 };

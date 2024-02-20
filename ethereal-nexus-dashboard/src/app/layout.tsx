442331df7e57eb6c5b3aca133dbfe348225890cc
Rui Silva <rui.silva@diconium.com>
2024-02-20 15:37:15 +0000

    feat: adds newrelic browser

--- ethereal-nexus-dashboard/src/app/layout.tsx
+++ ethereal-nexus-dashboard/src/app/layout.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Inter } from "next/font/google";
 import DashboardLayout from "@/components/layout";
 import { ThemeProvider } from '@/components/theme-provider';
+import NewRelicSnippet from '../components/newrelicSnippet'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,13 +12,17 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          <DashboardLayout>{children}</DashboardLayout>
-        </ThemeProvider>
-      </body>
-    </html>
-  );
+    const env = process.env.NODE_ENV
+
+    return <html lang="en">
+    { env == "production" ? (
+    <head>
+        <NewRelicSnippet/>
+    </head>): null}
+    <body className={inter.className}>
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <DashboardLayout>{children}</DashboardLayout>
+    </ThemeProvider>
+    </body>
+    </html>;
 }

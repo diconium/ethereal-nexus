65448ae168221366c2a63d21501f7a82d37bbaaa
Rui Silva <rui.silva@diconium.com>
2024-02-21 09:48:09 +0000

    fix: adds winston logs to console

--- ethereal-nexus-dashboard/src/logger.ts
+++ ethereal-nexus-dashboard/src/logger.ts
@@ -1,26 +1,15 @@
 import winston from "winston";
+const { combine, timestamp, json } = winston.format;
 
-/* Example logger with custom formatter
-https://github.com/winstonjs/winston#combining-formats
-const { combine, timestamp, label, prettyPrint } = winston.format;
 const logger = winston.createLogger({
-  format: combine(
-    label({ label: "NextJs NR Integration" }),
-    timestamp(),
-    prettyPrint()
-  ),
-  transports: [new winston.transports.Console()],
+    level: "info",
+    format: combine(timestamp(), json()),
+    transports: [
+        new winston.transports.Console(),
+        new winston.transports.File({
+            filename: "ethereal-nexus.log"
+        }),
+    ],
 });
-*/
-
-/* Example logger outputing to the console without formatting
-const logger = winston.createLogger({
-  transports: [new winston.transports.Console()],
-});
-*/
-
-// thsi will simply send logs to NR without outputting to the console
-// best for prod environments
-const logger = winston.createLogger();
 
 export { logger };

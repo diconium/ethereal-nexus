83cef0a2836e6f7f6a5cc665e44e1554cbc93727
Rui Silva <rui.silva@diconium.com>
2024-03-04 16:02:47 +0000

    feat: adds Get Components

--- ethereal-nexus-dashboard/src/app/api/v1/components/route.ts
+++ ethereal-nexus-dashboard/src/app/api/v1/components/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { HttpStatus } from '@/app/api/utils';
 import { authenticatedWithKey } from '@/lib/route-wrappers';
-import { upsertComponent } from '@/data/components/actions';
+import {getComponents, upsertComponent} from '@/data/components/actions';
 import {revalidatePath} from "next/cache";
 
 /**
@@ -64,6 +64,72 @@ export const POST = authenticatedWithKey(async (request: NextRequest, ext) => {
   }
 
   const componentWithVersion = await upsertComponent(req);
+
+  if (!componentWithVersion.success) {
+    return NextResponse.json(componentWithVersion.error, {
+      status: HttpStatus.BAD_REQUEST,
+    });
+  }
+  return NextResponse.json(componentWithVersion.data);
+});
+
+/**
+ * @swagger
+ * /api/v1/components:
+ *   get:
+ *     summary: Get all components
+ *     description: Get a list of all components
+ *     tags:
+ *      - Components
+ *     produces:
+ *      - application/json
+ *     requestBody:
+ *      required: true
+ *      content:
+ *       application/json:
+ *         schema:
+ *          type: object
+ *          $ref: '#/components/schemas/Component'
+ *     responses:
+ *      '200':
+ *        description: All components
+ *        content:
+ *         application/json:
+ *          schema:
+ *           type: array
+ *           items:
+ *           $ref: '#/components/schemas/Component'
+ *      '400':
+ *        description: Bad Request
+ *        content:
+ *         application/json:
+ *          schema:
+ *           type: object
+ *           properties:
+ *            message:
+ *             type: string
+ *             example: Bad Request - Invalid input data
+ *      '500':
+ *        description: Internal Server Error
+ *        content:
+ *         application/json:
+ *          schema:
+ *           type: object
+ *           properties:
+ *            message:
+ *             type: string
+ *             example: Internal Server Error - Something went wrong on the server side
+ */
+export const GET = authenticatedWithKey(async (request: NextRequest, ext) => {
+
+  const permissions = ext?.user.permissions;
+  if (permissions?.['components'] !== 'read') {
+    return NextResponse.json('You do not have permissions to write this resource.', {
+      status: HttpStatus.FORBIDDEN,
+    });
+  }
+
+  const componentWithVersion = await getComponents();
 
   if (!componentWithVersion.success) {
     return NextResponse.json(componentWithVersion.error, {

bb21197a9ce060a746d94945d3735121a9cd995d
SaraRDico <sara.rodrigues@diconium.com>
2024-03-23 16:35:44 +0000

    feat: add a ring loader

--- ethereal-nexus-dashboard/src/components/ui/loading.tsx
+++ ethereal-nexus-dashboard/src/components/ui/loading.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from 'react';
 import { useTheme } from 'next-themes';
+import { RingLoader } from 'react-spinners';
 
 export const Loading = ({
                           children,
@@ -13,18 +14,17 @@ export const Loading = ({
 
   useEffect(() => {
     if(resolvedTheme !== undefined) {
-      setLoading(false);
+      setTimeout(() => {
+        setLoading(false);
+      }, 1000); // 2 seconds delay
 
     }
-  }, []);
+  }, [resolvedTheme]);
 
   return loading ?
-    <div className="flex items-center justify-center w-full h-48 bg-gray-300 rounded sm:w-96 dark:bg-gray-700">
-      <svg className="w-10 h-10 text-gray-200 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-           fill="currentColor" viewBox="0 0 20 18">
-        <path
-          d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
-      </svg>
+    <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+      <RingLoader size={100} color={'#FF5600'} loading={loading} />
     </div>
-    : <div>{ children }</div>;
+
+    : <div>{children}</div>;
 };

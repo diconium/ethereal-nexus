20faeba2246f34eb25b75e2c16f74402fb1f543f
SaraRDico <sara.rodrigues@diconium.com>
2024-04-03 18:44:28 +0100

    feat: Add the possibility to have more projects styles

--- ethereal-nexus-dashboard/src/components/ui/update-table-view.tsx
+++ ethereal-nexus-dashboard/src/components/ui/update-table-view.tsx
@@ -28,23 +28,28 @@ export function UpdateTableView({ projects}) {
             projects.error.message
 
         ) :
-        <div className="bg-white border border-gray-300 rounded-lg max-w-xs w-full h-70 p-3">
+        <div className="flex">
           {projects.success ? (
             projects.data.map((project) => (
-              <div key={project.id} className="card">
-                <div className="bg-gradient-to-r from-orange-500 via-pink-500 to-purple-500 max-w-xs h-36 rounded-md"></div>
-                <span className="text-xs">{project.description}</span>
-                <h3 className="font-bold">{project.name}</h3>
-                <div className="border-t border-gray-200 my-2 opacity-40"></div>
-                <div className="flex items-center justify-center">
-                  <div className="flex flex-col items-center">
-                    <div className="text-xs">Components</div>
-                    <div className="text-orange-500 text-base font-bold">{project.components.length}</div>
-                  </div>
-                  <div className="mx-4 border-r border-gray-200 h-10 opacity-40"></div>
-                  <div className="flex flex-col items-center">
-                    <div className="text-xs">Project Members</div>
-                    <div className="text-orange-500 text-base font-bold">{project.membersLength}</div>
+              <div className="flex flex-row max-w-xs w-full h-70">
+                <div className="bg-white border border-gray-300 rounded-lg max-w-xs w-full h-70 p-3 mr-4">
+                  <div key={project.id} className="card ">
+                    <div
+                      className="bg-gradient-to-r from-orange-500 via-pink-500 to-purple-500 max-w-xs h-36 rounded-md"></div>
+                    <span className="text-xs">{project.description}</span>
+                    <h3 className="font-bold">{project.name}</h3>
+                    <div className="border-t border-gray-200 my-2 opacity-40"></div>
+                    <div className="flex items-center justify-center">
+                      <div className="flex flex-col items-center">
+                        <div className="text-xs">Components</div>
+                        <div className="text-orange-500 text-base font-bold">{project.components.length}</div>
+                      </div>
+                      <div className="mx-4 border-r border-gray-200 h-10 opacity-40"></div>
+                      <div className="flex flex-col items-center">
+                        <div className="text-xs">Project Members</div>
+                        <div className="text-orange-500 text-base font-bold">{project.membersLength}</div>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>

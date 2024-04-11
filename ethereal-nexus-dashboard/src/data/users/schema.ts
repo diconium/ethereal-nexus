98bafd803e8bba84cd06ad31a741aab99b92fb5e
Fabio Rito <fabio.rito@diconium.com>
2024-04-11 16:29:19 +0100

    fix(ethereal-nexus-dashboard): fix data type for api key permissions.

--- ethereal-nexus-dashboard/src/data/users/schema.ts
+++ ethereal-nexus-dashboard/src/data/users/schema.ts
@@ -1,30 +1,33 @@
-import { jsonb, pgEnum, pgTable, text, timestamp, uuid } from 'drizzle-orm/pg-core';
+import { pgEnum, pgTable, text, timestamp, uuid } from 'drizzle-orm/pg-core';
 import { v4 as uuidv4 } from 'uuid';
+import { customJsonb } from '@/db/custom-types';
 
 export const userRolesEnum = pgEnum('roles', ['admin', 'user', 'viewer']);
 
-export const users = pgTable("user", {
-  id: text("id").primaryKey().default(uuidv4()),
-  password: text("password"),
-  email: text("email").notNull().unique(),
-  email_verified: timestamp("emailVerified", { mode: "date" }),
-  name: text("name"),
-  image: text("image"),
-  role: userRolesEnum("role").notNull().default("user")
-})
+export const users = pgTable('user', {
+  id: text('id').primaryKey().default(uuidv4()),
+  password: text('password'),
+  email: text('email').notNull().unique(),
+  email_verified: timestamp('emailVerified', { mode: 'date' }),
+  name: text('name'),
+  image: text('image'),
+  role: userRolesEnum('role').notNull().default('user'),
+});
 
-export const apiKeys = pgTable("api_key", {
-  id: uuid("id").notNull().primaryKey().defaultRandom(),
-  key: uuid("key").notNull().unique().defaultRandom(),
-  user_id: text("user_id").notNull().references(() => users.id, { onDelete: 'cascade' }),
-  alias: text("alias"),
-  permissions: jsonb("permissions"),
-  created_at: timestamp("created_at").defaultNow().notNull(),
-})
+export const apiKeys = pgTable('api_key', {
+  id: uuid('id').notNull().primaryKey().defaultRandom(),
+  key: uuid('key').notNull().unique().defaultRandom(),
+  user_id: text('user_id')
+    .notNull()
+    .references(() => users.id, { onDelete: 'cascade' }),
+  alias: text('alias'),
+  permissions: customJsonb('permissions'),
+  created_at: timestamp('created_at').defaultNow().notNull(),
+});
 
-export const invites = pgTable("invite", {
-  id: uuid("id").notNull().primaryKey().defaultRandom(),
-  key: uuid("key").notNull().unique().defaultRandom(),
-  email: text("email").notNull(),
-  created_at: timestamp("created_at").defaultNow().notNull(),
-})
+export const invites = pgTable('invite', {
+  id: uuid('id').notNull().primaryKey().defaultRandom(),
+  key: uuid('key').notNull().unique().defaultRandom(),
+  email: text('email').notNull(),
+  created_at: timestamp('created_at').defaultNow().notNull(),
+});

2af45d8d46e2597bc136c29f925c9a39baad5148
Rui Silva <rui.silva@diconium.com>
2024-04-12 13:59:25 +0100

    fix: fixing urls with base

--- ethereal-nexus-site/src/components/logo-image-site.tsx
+++ ethereal-nexus-site/src/components/logo-image-site.tsx
@@ -2,21 +2,34 @@ import React, { useEffect, useState } from 'react';
 import { useStore } from '@nanostores/react';
 import { themeStore } from '@/lib/theme.ts';
 
+import lightModeImage from "../assets/ethereal-nexus-logo-light-mode.jpg";
+import darkModeImage from "../assets/ethereal-nexus-logo-dark-mode.jpg";
+
+
 export const LogoImageSite = () => {
   const $theme = useStore(themeStore);
-  const [image, setImage] = useState(`/ethereal-nexus-logo-${$theme}-mode.jpg`);
+  const [image, setImage] = useState(lightModeImage.src);
 
   useEffect(() => {
-    setImage(`/ethereal-nexus-logo-${$theme}-mode.jpg`);
+      if($theme === 'light') {
+        setImage(lightModeImage.src);
+      } else {
+        setImage(darkModeImage.src);
+      }
   }, [$theme]);
 
   return (
     <div>
-      <img
-        src={image}
-        alt="Ethereal Nexus Logo"
-        className="h-4 self-end justify-self-end"
-      />
+        {$theme == 'light' ? (<img
+            src={image}
+            alt="Ethereal Nexus Logo"
+            className="h-4 self-end justify-self-end"
+        />) : <img
+            src={image}
+            alt="Ethereal Nexus Logo"
+            className="h-4 self-end justify-self-end"
+        />}
+
     </div>
   );
 };

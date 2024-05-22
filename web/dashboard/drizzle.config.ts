16b8444718a2f2e09461b37c906fd887d6d51930
Francisco Madeira <francisco.madeira@diconium.com>
2024-05-22 18:17:41 +0100

    feat: update drizzle schema

--- web/dashboard/drizzle.config.ts
+++ web/dashboard/drizzle.config.ts
@@ -1,17 +1,18 @@
-import type { Config } from 'drizzle-kit';
 import * as dotenv from 'dotenv';
+import { defineConfig } from 'drizzle-kit'
 
 dotenv.config({
   path: '.env.local',
 });
 
-export default {
+export default defineConfig({
   schema: [
     "src/data/**/schema.ts",
   ],
-  out: "./drizzle",
-  driver: 'pg',
+  dialect: 'postgresql',
   dbCredentials: {
-    connectionString: process.env.DRIZZLE_DATABASE_URL!
+    url: process.env.DRIZZLE_DATABASE_URL!,
   },
-} satisfies Config;
+  verbose: true,
+  strict: true,
+})
--- web/dashboard/src/data/components/actions.ts
+++ web/dashboard/src/data/components/actions.ts
@@ -1,9 +1,9 @@
-'use server'
+'use server';
 
-import {ActionResponse, Result} from '@/data/action';
-import {z} from 'zod';
-import {actionError, actionSuccess, actionZodError} from '@/data/utils';
-import {db} from '@/db';
+import { ActionResponse, Result } from '@/data/action';
+import { z } from 'zod';
+import { actionError, actionSuccess, actionZodError } from '@/data/utils';
+import { db } from '@/db';
 
 import {
     Component,
@@ -18,17 +18,17 @@ import {
     componentVersionsSchema,
     ComponentWithVersion, NewComponentVersion
 } from './dto';
-import {and, eq, sql} from 'drizzle-orm';
+import { and, eq, sql } from 'drizzle-orm';
 import {
     componentAssets,
     components,
-    componentVersions,
+    componentVersions
 } from '@/data/components/schema';
-import {revalidatePath} from 'next/cache';
-import {projectComponentConfig, projects} from "@/data/projects/schema";
-import {projectWithOwners, ProjectWithOwners} from "@/data/projects/dto";
-import {members} from "@/data/member/schema";
-import {users} from "@/data/users/schema";
+import { revalidatePath } from 'next/cache';
+import { projectComponentConfig, projects } from '@/data/projects/schema';
+import { projectWithOwners, ProjectWithOwners } from '@/data/projects/dto';
+import { members } from '@/data/member/schema';
+import { users } from '@/data/users/schema';
 
 async function upsertComponentVersion(version: NewComponentVersion) {
     const safeVersion = componentVersionsCreateSchema.safeParse(version);
@@ -39,18 +39,18 @@ async function upsertComponentVersion(version: NewComponentVersion) {
         );
     }
 
-    const result  = await db
+    const result = await db
       .insert(componentVersions)
       .values(safeVersion.data)
       .onConflictDoUpdate({
           target: [
               componentVersions.component_id,
-              componentVersions.version,
+              componentVersions.version
           ],
           set: {
               dialog: safeVersion.data.dialog,
               readme: safeVersion.data.readme,
-              changelog: safeVersion.data.changelog,
+              changelog: safeVersion.data.changelog
           }
       })
       .returning();
@@ -69,13 +69,13 @@ async function upsertComponentVersion(version: NewComponentVersion) {
 }
 
 export async function upsertComponentWithVersion(
-    component: ComponentToUpsert,
+  component: ComponentToUpsert
 ): Promise<Result<ComponentWithVersion>> {
     const safeComponent = componentsUpsertSchema.safeParse(component);
     if (!safeComponent.success) {
         return actionZodError(
-            "There's an issue with the component record.",
-            safeComponent.error,
+          'There\'s an issue with the component record.',
+          safeComponent.error
         );
     }
     try {
@@ -87,23 +87,23 @@ export async function upsertComponentWithVersion(
               set: {
                   name: safeComponent.data.name,
                   title: safeComponent.data.title,
-                  description: safeComponent.data.description,
-              },
+                  description: safeComponent.data.description
+              }
           })
           .returning();
 
         const result = componentsSchema.safeParse(insertComponent[0]);
         if (!result.success) {
-            console.error(result.error)
+            console.error(result.error);
             return actionError('Failed to upsert component');
         }
-        const upsertedVersion=  await upsertComponentVersion({
+        const upsertedVersion = await upsertComponentVersion({
             component_id: result.data.id,
-            ...component,
+            ...component
         });
-        if(!upsertedVersion.success) {
+        if (!upsertedVersion.success) {
             return actionError(
-              upsertedVersion.error.message,
+              upsertedVersion.error.message
             );
         }
 
@@ -118,52 +118,58 @@ export async function upsertComponentWithVersion(
 }
 
 export async function upsertAssets(
-    componentId: string,
-    versionId: string,
-    url: string,
-    contentType: 'css' | 'js' | 'chunk' | null,
+  componentId: string,
+  versionId: string,
+  url: string,
+  contentType: 'css' | 'js' | 'chunk' | null
 ) {
     try {
         const assetToUpsert: z.infer<typeof componentAssetsCreateSchema> = {
             component_id: componentId,
             version_id: versionId!,
             url,
-            type: contentType,
+            type: contentType
         };
 
         const safeAssetToUpsert =
-            componentAssetsCreateSchema.safeParse(assetToUpsert);
+          componentAssetsCreateSchema.safeParse(assetToUpsert);
         if (!safeAssetToUpsert.success) {
             console.error(
-                'updateAssets: asset input is not valid',
-                JSON.stringify(safeAssetToUpsert, undefined, 2),
+              'updateAssets: asset input is not valid',
+              JSON.stringify(safeAssetToUpsert, undefined, 2)
             );
             return actionZodError(
-                "There's an issue with the components assets record.",
-                safeAssetToUpsert.error,
+              'There\'s an issue with the components assets record.',
+              safeAssetToUpsert.error
             );
         }
 
         const upsertedAsset = await db
           .insert(componentAssets)
           .values(safeAssetToUpsert.data)
-          .onConflictDoNothing()
+          .onConflictDoNothing({
+              target: [
+                  componentAssets.component_id,
+                  componentAssets.version_id,
+                  componentAssets.url
+              ]
+          })
           .returning();
 
-        if(upsertedAsset.length === 0) {
-            return actionError('Asset already exists.')
+        if (upsertedAsset.length === 0) {
+            return actionError('Asset already exists.');
         }
 
         const result = componentAssetsSchema.safeParse(upsertedAsset[0]);
         if (!result.success) {
             console.error(
               'updateAssets: failed to upsert asset',
-              JSON.stringify(result.error, undefined, 2),
+              JSON.stringify(result.error, undefined, 2)
             );
 
             return actionZodError(
-              "There's an issue with the inserted components assets record.",
-              result.error,
+              'There\'s an issue with the inserted components assets record.',
+              result.error
             );
         }
 
@@ -175,20 +181,20 @@ export async function upsertAssets(
 }
 
 export async function getComponents(): ActionResponse<
-    z.infer<typeof componentsWithVersions>[]
+  z.infer<typeof componentsWithVersions>[]
 > {
     try {
         const select = await db.query.components.findMany({
             with: {
-                versions: true,
-            },
+                versions: true
+            }
         });
 
         const safe = componentsWithVersions.array().safeParse(select);
         if (!safe.success) {
             return actionZodError(
-                "There's an issue with the components records.",
-                safe.error,
+              'There\'s an issue with the components records.',
+              safe.error
             );
         }
 
@@ -200,7 +206,7 @@ export async function getComponents(): ActionResponse<
 }
 
 export async function getComponentById(
-    id: string,
+  id: string
 ): ActionResponse<z.infer<typeof componentsSchema>> {
     if (!id) {
         return actionError('No identifier provided.');
@@ -208,14 +214,14 @@ export async function getComponentById(
 
     try {
         const select = await db.query.components.findFirst({
-            where: eq(components.id, id),
+            where: eq(components.id, id)
         });
 
         const safe = componentsSchema.safeParse(select);
         if (!safe.success) {
             return actionZodError(
-                "There's an issue with the component records.",
-                safe.error,
+              'There\'s an issue with the component records.',
+              safe.error
             );
         }
 
@@ -227,7 +233,7 @@ export async function getComponentById(
 }
 
 export async function getComponentVersions(
-    id: string,
+  id: string
 ): ActionResponse<z.infer<typeof componentVersionsSchema>[]> {
     if (!id) {
         return actionError('No identifier provided.');
@@ -235,13 +241,13 @@ export async function getComponentVersions(
 
     try {
         const select = await db.query.componentVersions.findMany({
-            where: eq(componentVersions.component_id, id),
+            where: eq(componentVersions.component_id, id)
         });
         const safe = componentVersionsSchema.array().safeParse(select);
         if (!safe.success) {
             return actionZodError(
-                "There's an issue with the component records.",
-                safe.error,
+              'There\'s an issue with the component records.',
+              safe.error
             );
         }
 
@@ -253,36 +259,37 @@ export async function getComponentVersions(
 }
 
 export async function getComponentDependentsProjectsWithOwners(
-    component_id: string,
+  component_id: string
 ): ActionResponse<ProjectWithOwners[]> {
     if (!component_id) {
         return actionError('No component id provided.');
     }
     try {
         const select = await db.select(
-            {
-                id: projects.id,
-                name: projects.name,
-                description: projects.description,
-                owners:  sql`ARRAY_AGG(${users.name})`
-            })
-            .from(projectComponentConfig)
-            .leftJoin(projects, eq(projects.id, projectComponentConfig.project_id))
-            .leftJoin(members,
-                and(
-                    eq(members.resource, projectComponentConfig.project_id),
-                    eq(members.role, 'owner')))
-            .leftJoin(users,eq(users.id,members.user_id))
-            .where(eq(projectComponentConfig.component_id, component_id))
-            .groupBy(
-               projects.id,
-            )
+          {
+              id: projects.id,
+              name: projects.name,
+              description: projects.description,
+              owners: sql`ARRAY_AGG
+                  (${users.name})`
+          })
+          .from(projectComponentConfig)
+          .leftJoin(projects, eq(projects.id, projectComponentConfig.project_id))
+          .leftJoin(members,
+            and(
+              eq(members.resource, projectComponentConfig.project_id),
+              eq(members.role, 'owner')))
+          .leftJoin(users, eq(users.id, members.user_id))
+          .where(eq(projectComponentConfig.component_id, component_id))
+          .groupBy(
+            projects.id
+          );
 
         const safe = projectWithOwners.array().safeParse(select);
         if (!safe.success) {
             return actionZodError(
-                "There's an issue with the component records.",
-                safe.error,
+              'There\'s an issue with the component records.',
+              safe.error
             );
         }
 
@@ -294,8 +301,8 @@ export async function getComponentDependentsProjectsWithOwners(
 }
 
 export async function getComponentAssets(
-    component_id: string,
-    version_id: string,
+  component_id: string,
+  version_id: string
 ): ActionResponse<z.infer<typeof componentAssetsSchema>[]> {
     if (!component_id) {
         return actionError('No component id provided.');
@@ -303,15 +310,15 @@ export async function getComponentAssets(
     try {
         const select = await db.query.componentAssets.findMany({
             where: and(
-                eq(componentAssets.component_id, component_id),
-                eq(componentAssets.version_id, version_id)
+              eq(componentAssets.component_id, component_id),
+              eq(componentAssets.version_id, version_id)
             )
         });
         const safe = componentAssetsSchema.array().safeParse(select);
         if (!safe.success) {
             return actionZodError(
-                "There's an issue with the component records.",
-                safe.error,
+              'There\'s an issue with the component records.',
+              safe.error
             );
         }
 
@@ -325,18 +332,18 @@ export async function getComponentAssets(
 export async function deleteComponent(id: string): ActionResponse<Component> {
     try {
         const del = await db.delete(components)
-            .where(eq(components.id, id))
-            .returning();
+          .where(eq(components.id, id))
+          .returning();
 
         const safe = componentsSchema.safeParse(del);
         if (!safe.success) {
             return actionZodError(
-                "There's an issue with the components records.",
-                safe.error,
+              'There\'s an issue with the components records.',
+              safe.error
             );
         }
 
-        revalidatePath('/components')
+        revalidatePath('/components');
         return actionSuccess(safe.data);
     } catch (error) {
         console.error(error);

54f8cdeeeec396f9c47900cbe0580b746072aaa2
Francisco Madeira <francisco.madeira@diconium.com>
2024-06-06 23:14:31 +0100

    refactor: update site to use more default components
    Signed-off-by: Francisco Madeira <francisco.madeira@diconium.com>

--- web/site/src/components/dashboard/openapi.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import 'swagger-ui-react/swagger-ui.css';
-import SwaggerUI from 'swagger-ui-react';
-export function DashboardOpenApi() {
-  return <SwaggerUI url="/dashboard/api_v1.json" />;
-}
--- web/site/src/content/config.ts
+++ web/site/src/content/config.ts
@@ -1,12 +1,6 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection } from 'astro:content';
 import { docsSchema } from '@astrojs/starlight/schema';
 
-const docs = defineCollection({
-  type: 'content',
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    api: z.string().optional(),
-  }),
-});
-export const collections = { docs: defineCollection({ schema: docsSchema() }) };
+export const collections = {
+	docs: defineCollection({ schema: docsSchema() }),
+};

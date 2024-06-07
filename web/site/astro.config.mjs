a296cde7623986afb47fa93545db636b296f7e23
Francisco Madeira <francisco.madeira@diconium.com>
2024-06-07 14:39:54 +0100

    refactor: update site to use more default components (#73)
    Signed-off-by: Francisco Madeira <francisco.madeira@diconium.com>

--- web/site/astro.config.mjs
+++ web/site/astro.config.mjs
@@ -1,30 +1,45 @@
 import { defineConfig } from 'astro/config';
-import mdx from "@astrojs/mdx";
-import react from '@astrojs/react';
-import icon from "astro-icon";
-import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
 import starlight from '@astrojs/starlight';
+import tailwind from '@astrojs/tailwind';
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://diconium.github.io',
-  base:"ethereal-nexus",
-  integrations: [
-    starlight({
-      title: 'My delightful docs site',
-      customCss: [
-        // Path to your Tailwind base styles:
-        './src/styles/globals.css',
-      ],
-    }),
-    react({
-    experimentalReactChildren: true
-  }),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    icon(),
-    sitemap(),
-  ]
+  base: 'ethereal-nexus',
+  integrations: [starlight({
+    components: {
+      SiteTitle: './src/components/starlight/SiteTitle.astro',
+      Hero: './src/components/starlight/Hero.astro'
+    },
+    title: 'Docs with Tailwind',
+    social: {
+      github: 'https://github.com/withastro/starlight'
+    },
+    sidebar: [{
+      label: 'Getting Started',
+      autogenerate: {
+        directory: 'setup'
+      }
+    }, {
+      label: 'Connectors',
+      autogenerate: {
+        directory: 'connectors'
+      }
+    }, {
+      label: 'Dashboard',
+      autogenerate: {
+        directory: 'dashboard'
+      }
+    }, {
+      label: 'Reference',
+      autogenerate: {
+        directory: 'reference'
+      }
+    }],
+    customCss: ['./src/tailwind.css']
+  }), tailwind({
+    applyBaseStyles: false
+  }), react()]
 });
--- web/site/src/lib/theme.ts
+++
@@ -1,8 +0,0 @@
-import { atom } from 'nanostores';
-
-export enum Theme {
-  LIGHT = 'light',
-  DARK = 'dark',
-}
-
-export const themeStore = atom(Theme.LIGHT);

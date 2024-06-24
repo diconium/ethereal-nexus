17f6973d2940187e40469dfc159b9355e5e5db4a
Rui.Silva@diconium.com <Rui.Silva@diconium.com>
2024-06-24 11:13:22 +0100

    fix: adds Access-Control-Allow-Origin * to api calls.

--- web/dashboard/next.config.js
+++ web/dashboard/next.config.js
@@ -3,5 +3,16 @@ module.exports = {
   output: "standalone",
   experimental: {
     serverMinification: false
-  }
+  },
+  headers: async () => {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+        ]
+      }
+    ]
+}
 };

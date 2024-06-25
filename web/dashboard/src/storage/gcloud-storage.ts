1c56e6380e858b9e595a0bf659de1ff6596ae4c1
Sandra Teixeira <56432861+teixesan@users.noreply.github.com>
2024-06-25 11:45:01 +0100

    Feature/gcloud (#74)
    * added gcloud support
    
    * added gcloud support
    
    * fix: ssr assets loaded on server to be the server.js file.
    
    * add gcloud-key.json to gitignore.
    
    * added gcloud support
    
    * added gcloud support
    
    * add gcloud-key.json to gitignore.
    
    * pnpm-lock update
    
    * added env example
    
    * adds azure explicit as default.
    
    * fix: azure cloud storage.
    
    ---------
    
    Co-authored-by: Rui.Silva@diconium.com <Rui.Silva@diconium.com>

---
+++ web/dashboard/src/storage/gcloud-storage.ts
@@ -0,0 +1,31 @@
+import path from 'node:path';
+import { Storage } from '@google-cloud/storage';
+import { IStorage } from '@/storage/ethereal-storage';
+
+const GCLOUD_BLOB_STORAGE_ID = process.env.GCLOUD_BLOB_STORAGE_ID || '';
+const GCLOUD_BLOB_STORAGE_KEY_FILE= process.env.GCLOUD_BLOB_STORAGE_KEY_FILE || '';
+const GCLOUD_BLOB_STORAGE_NAME = process.env.GCLOUD_BLOB_STORAGE_NAME || '';
+
+export class GcloudStorage implements IStorage {
+
+    private storage: Storage;
+
+    constructor() {
+       this.storage = new Storage({
+        keyFilename: path.basename(GCLOUD_BLOB_STORAGE_KEY_FILE), // Path to your service account key file
+        projectId: GCLOUD_BLOB_STORAGE_ID, // Your Google Cloud project ID
+      });
+    }
+
+    async uploadToStorage(code: string, name: string, file: string): Promise<URL> {
+      const fileName = path.join(name, file);
+      const bucket = this.storage.bucket(GCLOUD_BLOB_STORAGE_NAME);
+      await bucket.file(fileName).save(code);
+
+      const url = new URL(`${process.env.GCLOUD_CDN_URL}`);
+      url.pathname=fileName;
+
+      return url;
+    }
+
+}

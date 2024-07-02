1db2d8974b74073a3637a1b89c70e6c3d7f0319e
Francisco Madeira <francisco.madeira@diconium.com>
2024-07-02 11:28:37 +0100

    fix: move project name to top of folder
    Signed-off-by: Francisco Madeira <francisco.madeira@diconium.com>

--- web/dashboard/src/app/(session)/projects/[id]/page.tsx
+++ web/dashboard/src/app/(session)/projects/[id]/page.tsx
@@ -6,7 +6,7 @@ import { notFound } from 'next/navigation';
 import { ProjectMemberList } from '@/components/projects/members-table/member-list';
 import { ProjectComponentsList } from '@/components/projects/component-selection-table/components-list';
 import Link from 'next/link';
-import { Overview } from '@/components/projects/overview';
+import ProjectsForm from '@/components/projects/project-form';
 
 export default async function EditProject({ params: { id }, searchParams: { tab } }: any) {
   const session = await auth();
@@ -17,14 +17,15 @@ export default async function EditProject({ params: { id }, searchParams: { tab 
   }
 
   return (
-    <div className="container space-y-6">
-      <Tabs value={tab} defaultValue="overview" className="space-y-10 mt-6">
+    <div className="container h-full flex-1 flex-col space-y-8 p-8 md:flex">
+      <div className="flex items-center justify-between space-y-2">
+        <div>
+          <h2 className="text-2xl font-bold tracking-tight">{project.data.name}</h2>
+          <p className="text-muted-foreground">{project.data.description}</p>
+        </div>
+      </div>
+      <Tabs value={tab} defaultValue="components" className="space-y-10 mt-6">
         <TabsList className="w-[21.5rem]">
-          <TabsTrigger value="overview" asChild>
-            <Link href={`/projects/${id}?tab=overview`}>
-              Overview
-            </Link>
-          </TabsTrigger>
           <TabsTrigger value="components" asChild>
             <Link href={`/projects/${id}?tab=components`}>
               Components
@@ -35,20 +36,25 @@ export default async function EditProject({ params: { id }, searchParams: { tab 
               Users
             </Link>
           </TabsTrigger>
+          <TabsTrigger value="settings" asChild>
+            <Link href={`/projects/${id}?tab=settings`}>
+              Settings
+            </Link>
+          </TabsTrigger>
         </TabsList>
-        <TabsContent value="overview" className="py-6">
-          <Overview
-            project={project.data}
-          />
-        </TabsContent>
-        <TabsContent value="components" className="space-y-4 p-6">
+        <TabsContent value="components" className="space-y-4">
           <ProjectComponentsList
             id={id}
           />
         </TabsContent>
-        <TabsContent value="users" className="space-y-4 p-6">
+        <TabsContent value="users" className="space-y-4">
           <ProjectMemberList
             id={id}
+          />
+        </TabsContent>
+        <TabsContent value="settings">
+          <ProjectsForm
+            project={project.data}
           />
         </TabsContent>
       </Tabs>
--- web/dashboard/src/components/ui/tabs.tsx
+++ web/dashboard/src/components/ui/tabs.tsx
@@ -29,7 +29,7 @@ const TabsTrigger = React.forwardRef<
   <TabsPrimitive.Trigger
     ref={ref}
     className={cn(
-      "inline-flex items-center justify-center whitespace-nowrap rounded-full px-4 h-9 text-base font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-orange-500 data-[state=active]:text-white data-[state=active]:shadow data-[state=active]:font-bold",
+      "inline-flex items-center justify-center whitespace-nowrap rounded-full px-4 h-9 text-base font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-orange-500 data-[state=active]:text-white data-[state=active]:shadow",
       className
     )}
     {...props}

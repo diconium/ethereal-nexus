0fc43fa57d7b25d22c83f3732e117164381f3416
Rui.Silva@diconium.com <Rui.Silva@diconium.com>
2024-07-18 10:54:54 +0100

    fix: users id is now a uuid on the db.

--- web/dashboard/src/data/users/schema.ts
+++ web/dashboard/src/data/users/schema.ts
@@ -1,11 +1,10 @@
 import { pgEnum, pgTable, text, timestamp, uuid } from 'drizzle-orm/pg-core';
-import { v4 as uuidv4 } from 'uuid';
 import { customJsonb } from '@/db/custom-types';
 
 export const userRolesEnum = pgEnum('roles', ['admin', 'user', 'viewer']);
 
 export const users = pgTable('user', {
-  id: text('id').primaryKey().default(uuidv4),
+  id: uuid('id').notNull().primaryKey().defaultRandom(),
   password: text('password'),
   email: text('email').notNull().unique(),
   email_verified: timestamp('emailVerified', { mode: 'date' }),
@@ -17,7 +16,7 @@ export const users = pgTable('user', {
 export const apiKeys = pgTable('api_key', {
   id: uuid('id').notNull().primaryKey().defaultRandom(),
   key: uuid('key').notNull().unique().defaultRandom(),
-  user_id: text('user_id')
+  user_id: uuid('user_id')
     .notNull()
     .references(() => users.id, { onDelete: 'cascade' }),
   alias: text('alias'),

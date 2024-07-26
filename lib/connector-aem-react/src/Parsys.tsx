f1e63bb9c5d6d24585371c4f6b38bbdd32150709
Rui.Silva@diconium.com <Rui.Silva@diconium.com>
2024-07-26 08:39:02 +0100

    wip

---
+++ lib/connector-aem-react/src/Parsys.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+interface DynamicZoneProps {
+  slot?: {
+    childrenHtml?: string;
+    dataPath?: string;
+    dataConfig?: any;
+  };
+}
+
+const Parsys: React.FC<DynamicZoneProps> = ({ slot }) => {
+  return (
+    <>
+      {slot && slot.childrenHtml && (
+        <div className={'new'}>
+          <div dangerouslySetInnerHTML={{ __html: atob(slot.childrenHtml) }} />
+        </div>
+      )}
+      {slot && slot.dataPath && slot.dataConfig && (
+        <div className={'new'}>
+          {/*// @ts-ignore*/}
+          <cq data-path={slot.dataPath} data-config={JSON.stringify(slot.dataConfig)}></cq>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Parsys
---
+++ lib/connector-aem-react/tsup.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  target: 'es2020',
+  format: ['cjs', 'esm'],
+  splitting: false,
+  sourcemap: true,
+  clean: true,
+  dts: true,
+  outDir: './dist',
+})

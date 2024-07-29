69746bb68ac1325f5ca15cd58c4972671fb3d127
Rui.Silva@diconium.com <Rui.Silva@diconium.com>
2024-07-29 10:25:51 +0100

    fix: add custom `cq` element to `JSX.IntrinsicElements` to resolve TS2339 error
    - Declared `cq` element in `JSX.IntrinsicElements` interface.
    - Updated `Parsys` component to use the custom `cq` element.

--- lib/connector-aem-react/src/Parsys.tsx
+++ lib/connector-aem-react/src/Parsys.tsx
@@ -1,5 +1,13 @@
 import React from "react";
 
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      cq: React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+    }
+  }
+}
+
 interface DynamicZoneProps {
   slot?: {
     childrenHtml?: string;
@@ -25,4 +33,4 @@ const Parsys: React.FC<DynamicZoneProps> = ({ slot }) => {
   );
 };
 
-export default Parsys
+export default Parsys;
--- lib/connector-aem-react/src/react-app-env.d.ts
+++
@@ -1,5 +0,0 @@
-declare namespace JSX {
-  interface IntrinsicElements {
-    cq: React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
-  }
-}

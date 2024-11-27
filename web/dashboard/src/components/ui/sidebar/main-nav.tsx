a4526bb933b10785c87988d057837ff6b7979b0d
Francisco Madeira <francisco.madeira@diconium.com>
2024-11-27 00:16:07 +0000

    feat: add user group to sidebar

--- web/dashboard/src/components/ui/sidebar/main-nav.tsx
+++ web/dashboard/src/components/ui/sidebar/main-nav.tsx
@@ -1,18 +1,24 @@
 'use client';
 
-import { type LucideIcon } from 'lucide-react';
-
-
 import { SidebarGroup } from '@/components/ui/sidebar';
 import Link from 'next/link';
+import { Folder, LayoutDashboard, LayoutGrid, UserRound } from 'lucide-react';
+import { createElement } from 'react';
 
 interface NavItem {
   items: {
     title: string
     url: string
-    icon?: LucideIcon
+    icon?: string
     isActive?: boolean
   }[];
+}
+
+const icons = {
+  LayoutDashboard: LayoutDashboard,
+  UserRound: UserRound,
+  LayoutGrid: LayoutGrid,
+  Folder: Folder,
 }
 
 export function NavMain({ items }: NavItem) {
@@ -22,12 +28,11 @@ export function NavMain({ items }: NavItem) {
         <Link
           href={item.url}
           key={item.title}
-          className="flex items-center gap-2 p-2 rounded-xl active:bg-orange-500"
+          className="flex items-center gap-2 p-2 rounded-xl active:bg-orange-500 hover:bg-sidebar-accent hover:text-sidebar-accent-foreground hover:shadow-[0_0_0_1px_hsl(var(--sidebar-accent))]"
         >
-          <span className="p-2 rounded-xl bg-orange-700">{item.icon && <item.icon />} </span>
+          <span className="p-2 rounded-xl bg-orange-700">{item.icon && createElement(icons[item.icon])} </span>
           <span>{item.title}</span>
         </Link>
-
       ))}
     </SidebarGroup>
   );

a5e4d2d7e8ba51c475921e6857565115d8d37c7d
Francisco Madeira <francisco.madeira@diconium.com>
2025-02-03 18:18:32 +0000

    feat: update font type to poppins

--- web/dashboard/src/app/layout.tsx
+++ web/dashboard/src/app/layout.tsx
@@ -1,12 +1,19 @@
 import "./globals.css";
+import { Poppins } from 'next/font/google'
+
+const poppins = Poppins({
+  weight: ['600', '400'],
+  subsets: ['latin'],
+})
+
 import React from "react";
 import { ThemeProvider } from '@/components/theme-provider';
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
-  return <html lang="en" suppressHydrationWarning>
+  return <html lang="en" className={poppins.className} suppressHydrationWarning>
     <head>
     </head>
-    <body className="font-campton">
+    <body>
       <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
         {children}
       </ThemeProvider>

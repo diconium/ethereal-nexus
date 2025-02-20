05e89200bc40cf2891bd9c9057f97c1c680c4cfa
teixesan <sandra.teixeira@diconium.com>
2025-02-20 14:13:15 +0000

    fix: prop drill when new components were added in the same slot

--- lib/react/src/Slot/Slot.tsx
+++ lib/react/src/Slot/Slot.tsx
@@ -4,6 +4,8 @@ type SlotProps = HTMLAttributes<HTMLSlotElement> & {
   [key: string]: any; // Spreadable props for the slotted element
 }
 
+const CLASS_IN_EDIT_MODE = "cq-Editable-dom--container";
+
 export const Slot: React.FC<SlotProps> = ({ name, ...props }) => {
   const slotRef = useRef<HTMLSlotElement>(null);
 
@@ -11,30 +13,52 @@ export const Slot: React.FC<SlotProps> = ({ name, ...props }) => {
     const slot = slotRef.current;
     if (!slot) return;
 
+
     const applyProps = () => {
       const dynamicZone = slot.assignedElements();
       Array.from(dynamicZone[0].children)
         // workaround for cq-Editable-dom in AEM editor mode that generates a new div for the Editor components
-        .map(el => el.classList.value.includes("cq-Editable-dom--container") ? Array.from(el.children) : el)
+        .map(el => el.classList.value.includes(CLASS_IN_EDIT_MODE) ? Array.from(el.children) : el)
         .flat()
         .filter(el => el.tagName.includes("-"))
         .forEach(el =>
-            Object.keys(props)
-              .forEach(key => {
+          Object.keys(props)
+            .forEach(key => {
                 const value = typeof props[key] === 'object' ?
                   JSON.stringify(props[key]) :
                   props[key];
                 el.setAttribute(key, value)
               }
-          )
+            )
         )
     };
 
-    slot.addEventListener("slotchange", applyProps);
+    const observeClassChanges = () => {
+      const assignedNodes = slot.assignedElements();
+
+      if (!assignedNodes?.length) return;
+
+      const [firstNode] = assignedNodes;
+
+      classObserver?.observe(firstNode, { attributes: true, attributeFilter: ["class"], subtree: true });
+    };
+
+    const classObserver = new MutationObserver((mutationsList) => {
+      mutationsList.forEach(mutation =>  {
+        if (mutation.type === "attributes"
+          && mutation.attributeName === "class"
+          && mutation.target instanceof Element
+          && mutation.target.classList?.contains(CLASS_IN_EDIT_MODE)) {
+            applyProps();
+        }
+      })
+    });
+
     applyProps();
+    observeClassChanges()
 
     return () => {
-      slot.removeEventListener("slotchange", applyProps);
+      classObserver.disconnect();
     };
   }, [props]);
 

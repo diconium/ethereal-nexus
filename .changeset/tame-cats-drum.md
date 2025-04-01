b7043ecfa3fe90b9954238b9b15fe1553d14c79a
teixesan <sandra.teixeira@diconium.com>
2025-04-01 13:24:57 +0100

    fix: prop drill inside experience fragments

---
+++ .changeset/tame-cats-drum.md
@@ -0,0 +1,5 @@
+---
+'@ethereal-nexus/react': patch
+---
+
+Changed slot to allow the prop drill to nexus components inside experience fragments
--- lib/react/src/Slot/Slot.tsx
+++ lib/react/src/Slot/Slot.tsx
@@ -5,32 +5,49 @@ type SlotProps = HTMLAttributes<HTMLSlotElement> & {
 }
 
 const CLASS_IN_EDIT_MODE = "cq-Editable-dom";
+const XP_FRAGMENT = "cmp-experiencefragment";
 
 export const Slot: React.FC<SlotProps> = ({ name, ...props }) => {
   const slotRef = useRef<HTMLSlotElement>(null);
 
+
   useEffect(() => {
     const slot = slotRef.current;
+
     if (!slot) return;
 
+    const assignedNodes = slot.assignedElements();
 
-    const applyProps = () => {
-      const dynamicZone = slot.assignedElements();
+    const processElement = (el: Element)=> {
+      /**
+       * Workaround to apply props to all available nexus components inside an AEM Experience Fragment.
+       * The components are wrapped in a div with the class "cmp-experiencefragment > div > div > div".
+       */
+      if (el.classList.value.includes(XP_FRAGMENT)) {
+        const nexusComponents = Array.from(el?.children?.[0]?.children?.[0]?.children);
+        applyProps(nexusComponents);
+        return;
+      }
+
+      Object.keys(props)
+        .forEach((key) => {
+            const value = typeof props[key] === 'object' ?
+              JSON.stringify(props[key]) :
+              props[key];
+            el.setAttribute(key, value);
+          },
+        );
+    }
+
+    const applyProps = (dynamicZone: Element[]) => {
+      if (!dynamicZone?.length) return;
+
       Array.from(dynamicZone[0].children)
-        // workaround for cq-Editable-dom in AEM editor mode that generates a new div for the Editor components
-        .map(el => el.classList.value.includes(CLASS_IN_EDIT_MODE) ? Array.from(el.children) : el)
+        // workaround for cq-Editable-dom  in AEM editor mode that generates a new div for the Editor components
+        .map(el => el.classList.contains(CLASS_IN_EDIT_MODE) ? Array.from(el.children) : el)
         .flat()
-        .filter(el => el.tagName.includes("-"))
-        .forEach(el =>
-          Object.keys(props)
-            .forEach(key => {
-                const value = typeof props[key] === 'object' ?
-                  JSON.stringify(props[key]) :
-                  props[key];
-                el.setAttribute(key, value)
-              }
-            )
-        )
+        .filter(el => el.tagName.includes("-") || el.classList.contains(XP_FRAGMENT))
+        .forEach(processElement)
     };
 
     const observeClassChanges = () => {
@@ -48,13 +65,13 @@ export const Slot: React.FC<SlotProps> = ({ name, ...props }) => {
         if (mutation.type === "attributes"
           && mutation.attributeName === "class"
           && mutation.target instanceof Element
-          && mutation.target.classList?.contains(CLASS_IN_EDIT_MODE)) {
-            applyProps();
+          && (mutation.target.classList?.contains(CLASS_IN_EDIT_MODE))) {
+            applyProps(assignedNodes);
         }
       })
     });
 
-    applyProps();
+    applyProps(assignedNodes);
     observeClassChanges()
 
     return () => {

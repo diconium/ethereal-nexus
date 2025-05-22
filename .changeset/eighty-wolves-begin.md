a5a6b3a130218c584f5fa6a8db6aec1248e0a1e2
teixesan <sandra.teixeira@diconium.com>
2025-05-22 15:18:39 +0100

    feat: Introduced configurable options in the pathbrowser to enable folder-only selection.

---
+++ .changeset/eighty-wolves-begin.md
@@ -0,0 +1,6 @@
+---
+'remote-components-vite-react-ts': minor
+'@ethereal-nexus/core': minor
+---
+
+Introduced configurable options in the pathbrowser to enable folder-only selection.
--- examples/vite-react-ts/src/components/SimpleReactHelloWorld/SimpleReactHelloWorld.tsx
+++ examples/vite-react-ts/src/components/SimpleReactHelloWorld/SimpleReactHelloWorld.tsx
@@ -4,8 +4,9 @@ import {
   text,
   dialog,
   type Output,
+  pathbrowser,
+  calendar,
 } from '@ethereal-nexus/core';
-
 const dialogSchema = dialog({
   title: text({
     label: 'Title',
@@ -15,18 +16,48 @@ const dialogSchema = dialog({
     validationRegex: '^Hello World$',
     validationErrorMessage: 'The text must be "Hello World"',
   }),
+  asset_folder: pathbrowser({
+    label: 'Test',
+    tooltip: 'This is a test',
+    placeholder: 'Select a path',
+    folder: true,
+    path: "/content/dam/",
+  }),
+  other_folder: pathbrowser({
+    label: 'Test',
+    tooltip: 'This is a test',
+    placeholder: 'Select a path',
+    folder: false,
+    path: "/content",
+  }),
+  default_pathbrowser: pathbrowser({
+    label: 'Test',
+    tooltip: 'This is a test',
+    placeholder: 'Select a path'
+  }),
+  calendar: calendar({
+    label: 'Calendar',
+    valueformat: "YYYY-MM-DD_HH:mmZ",
+    displayformat: "YYYY-MM-DD",
+    headerformat: "MMMM YYYY",
+  })
 });
 
-const schema = component({ version: '0.0.3' }, dialogSchema);
+const schema = component({ version: '0.0.6' }, dialogSchema);
 
 type Props = Output<typeof schema>
 
-export const SimpleReactHelloWorld: React.FC<Props> = ({ title }) => {
+export const SimpleReactHelloWorld: React.FC<Props> = ({ title, asset_folder, default_pathbrowser, other_folder }) => {
 
   return (
     <div>
       My new text Hello World from react!
       <blockquote>{title}</blockquote>
+      <div>
+        <p>Asset Folder: {asset_folder}</p>
+        <p>Default Pathbrowser: {default_pathbrowser}</p>
+        <p>Other Folder: {other_folder}</p>
+      </div>
     </div>
   );
 };

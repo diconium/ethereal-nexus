7920d568766873421df5fa5a99c22fa1af005228
teixesan <sandra.teixeira@diconium.com>
2026-05-29 15:41:51 +0100

    feat:add description field to component interface

---
+++ .changeset/good-breads-march.md
@@ -0,0 +1,5 @@
+---
+'@ethereal-nexus/core': minor
+---
+
+add description field to component interface
--- lib/core/src/types/component.ts
+++ lib/core/src/types/component.ts
@@ -2,6 +2,7 @@ export type ComponentModel = {
   name: string,
   title: string,
   slug: string,
+  description: string,
   version: string,
   readme: string,
 }
